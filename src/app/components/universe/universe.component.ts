f6818bba4441c1f096e3dd058ccf9ca747636908
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2022-08-12 09:41:38 +0200

    Test wasm with greet function

--- src/app/components/universe/universe.component.ts
+++ src/app/components/universe/universe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { greet } from 'wasm-universe';
 
 @Component({
   selector: 'simulation',
@@ -10,6 +11,7 @@ export class UniverseComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    greet();
   }
 
 }
