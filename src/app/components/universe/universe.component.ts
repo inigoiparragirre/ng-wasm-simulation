7960a51006515d4c42b31e76db25f531f02d1711
Iñigo Iparragirre <inigoiparraguirre@gmail.com>
2022-08-12 09:27:35 +0200

    Add universe component

---
+++ src/app/components/universe/universe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'simulation',
+  templateUrl: './universe.component.html',
+  styleUrls: ['./universe.component.scss']
+})
+export class UniverseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
